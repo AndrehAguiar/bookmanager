c47aa127ec5fd5ae59c12573dbfe66470ad5f873
AndrehAguiar <andreaguiar.g@gmail.com>
2021-07-02 20:28:55 -0300

    First commit: project struct created

---
+++ src/test/java/com/bookstore/bookmanager/BookmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
