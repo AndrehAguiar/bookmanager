ed5220f9e40c22b116b092cd14cc413095c2a531
AndrehAguiar <andreaguiar.g@gmail.com>
2021-07-02 21:18:26 -0300

    entities created

---
+++ src/main/java/com/bookstore/bookmanager/entity/Author.java
@@ -0,0 +1,17 @@
+package com.bookstore.bookmanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
---
+++ src/main/java/com/bookstore/bookmanager/entity/Book.java
@@ -0,0 +1,29 @@
+package com.bookstore.bookmanager.entity;
+
+import javax.persistence.*;
+
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name="PUBLISHER_NAME", nullable = false, unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
+    private Author author;
+}
